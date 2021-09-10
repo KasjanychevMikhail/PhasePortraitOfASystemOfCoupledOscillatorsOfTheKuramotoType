1ce30cc84405372820bed827cf401f1119646a40
KasjanychevMikhail <43135863+KasjanychevMikhail@users.noreply.github.com>
2021-09-10 23:32:57 +0300

    ver4

--- main.py
+++ main.py
@@ -2,63 +2,68 @@ import numpy as np
 from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
 
-def rhs(s, v):
-    return [-1 + 0.2 * ((-np.sin(v[0] - v[0] + 1.6) + 0.2 * np.sin(2 * (v[0] - v[0]) - 1.58)) +
-                       (-np.sin(v[0] - v[1] + 1.6) + 0.2 * np.sin(2 * (v[0] - v[1]) - 1.58)) +
-                       (-np.sin(v[0] - v[2] + 1.6) + 0.2 * np.sin(2 * (v[0] - v[2]) - 1.58)) +
-                       (-np.sin(v[0] - v[3] + 1.6) + 0.2 * np.sin(2 * (v[0] - v[3]) - 1.58)) +
-                       (-np.sin(v[0] - v[4] + 1.6) + 0.2 * np.sin(2 * (v[0] - v[4]) - 1.58))),
-            0 + 0.2 * ((-np.sin(v[1] - v[0] + 1.6) + 0.2 * np.sin(2 * (v[1] - v[0]) - 1.58)) +
-                       (-np.sin(v[1] - v[1] + 1.6) + 0.2 * np.sin(2 * (v[1] - v[1]) - 1.58)) +
-                       (-np.sin(v[1] - v[2] + 1.6) + 0.2 * np.sin(2 * (v[1] - v[2]) - 1.58)) +
-                       (-np.sin(v[1] - v[3] + 1.6) + 0.2 * np.sin(2 * (v[1] - v[3]) - 1.58)) +
-                       (-np.sin(v[1] - v[4] + 1.6) + 0.2 * np.sin(2 * (v[1] - v[4]) - 1.58))),
-            1 + 0.2 * ((-np.sin(v[2] - v[0] + 1.6) + 0.2 * np.sin(2 * (v[2] - v[0]) - 1.58)) +
-                       (-np.sin(v[2] - v[1] + 1.6) + 0.2 * np.sin(2 * (v[2] - v[1]) - 1.58)) +
-                       (-np.sin(v[2] - v[2] + 1.6) + 0.2 * np.sin(2 * (v[2] - v[2]) - 1.58)) +
-                       (-np.sin(v[2] - v[3] + 1.6) + 0.2 * np.sin(2 * (v[2] - v[3]) - 1.58)) +
-                       (-np.sin(v[2] - v[4] + 1.6) + 0.2 * np.sin(2 * (v[2] - v[4]) - 1.58))),
-            2 + 0.2 * ((-np.sin(v[3] - v[0] + 1.6) + 0.2 * np.sin(2 * (v[3] - v[0]) - 1.58)) +
-                       (-np.sin(v[3] - v[1] + 1.6) + 0.2 * np.sin(2 * (v[3] - v[1]) - 1.58)) +
-                       (-np.sin(v[3] - v[2] + 1.6) + 0.2 * np.sin(2 * (v[3] - v[2]) - 1.58)) +
-                       (-np.sin(v[3] - v[3] + 1.6) + 0.2 * np.sin(2 * (v[3] - v[3]) - 1.58)) +
-                       (-np.sin(v[3] - v[4] + 1.6) + 0.2 * np.sin(2 * (v[3] - v[4]) - 1.58))),
-            3 + 0.2 * ((-np.sin(v[4] - v[0] + 1.6) + 0.2 * np.sin(2 * (v[4] - v[0]) - 1.58)) +
-                       (-np.sin(v[4] - v[1] + 1.6) + 0.2 * np.sin(2 * (v[4] - v[1]) - 1.58)) +
-                       (-np.sin(v[4] - v[2] + 1.6) + 0.2 * np.sin(2 * (v[4] - v[2]) - 1.58)) +
-                       (-np.sin(v[4] - v[3] + 1.6) + 0.2 * np.sin(2 * (v[4] - v[3]) - 1.58)) +
-                       (-np.sin(v[4] - v[4] + 1.6) + 0.2 * np.sin(2 * (v[4] - v[4]) - 1.58)))]
+class FiveOscillators:
+    def __init__(self, paramW, paramA, paramB, paramR):
+        self.paramW = paramW
+        self.paramA = paramA
+        self.paramB = paramB
+        self.paramR = paramR
 
-def drawPhasePortrait(args):
-    res = solve_ivp(rhs, (args[0], args[1]), [4.8, 3, 2.2, 0, 1])
-    axs[0, 0].plot(res.y[0].T, res.y[1].T, 'b')
-    axs[0, 1].plot(res.y[0].T, res.y[2].T, 'b')
-    axs[0, 2].plot(res.y[0].T, res.y[4].T, 'b')
-    axs[1, 0].plot(res.y[1].T, res.y[2].T, 'b')
-    axs[1, 1].plot(res.y[1].T, res.y[4].T, 'b')
-    axs[1, 2].plot(res.y[2].T, res.y[4].T, 'b')
+    def func(self, phi):
+        return -np.sin(phi + self.paramA) + self.paramR * np.sin(2 * phi + self.paramB)
 
-    axs[0, 0].set(xlabel='theta1')
-    axs[0, 0].set(ylabel='theta2')
-    axs[0, 0].grid()
-    axs[0, 1].set(xlabel='theta1')
-    axs[0, 1].set(ylabel='theta3')
-    axs[0, 1].grid()
-    axs[0, 2].set(xlabel='theta1')
-    axs[0, 2].set(ylabel='theta5')
-    axs[0, 2].grid()
-    axs[1, 0].set(xlabel='theta2')
-    axs[1, 0].set(ylabel='theta3')
-    axs[1, 0].grid()
-    axs[1, 1].set(xlabel='theta2')
-    axs[1, 1].set(ylabel='theta5')
-    axs[1, 1].grid()
-    axs[1, 2].set(xlabel='theta3')
-    axs[1, 2].set(ylabel='theta5')
-    axs[1, 2].grid()
+    def getFullSystem(self, phis):
+        resPhis = [0, 0, 0, 0, 0]
+        for i in range(5):
+            tmp = self.paramW
+            for j in range(5):
+                tmp += 0.20 * self.func(phis[i] - phis[j])
+            resPhis[i] = tmp
+        return resPhis
 
-args = (1000, 10000)
+    def getReducedSystem(self, t, gammas):
+        gammas = list(gammas)
+        phis = gammas + [0.]
+        resPhi = self.getFullSystem(phis)
+        resGamma = [0, 0, 0, 0, 0]
+        for i in range(5):
+            resGamma[i] = resPhi[i] - resPhi[4]
+        return resGamma[:-1]
+
+    def getParams(self):
+        return[self.paramW, self.paramA, self.paramB, self.paramR]
+
+    def drawPhasePortrait(self, args):
+        res = solve_ivp(self.getReducedSystem, (args[0], args[1]), [3.6, 1.7, 1.2, -0.2])
+        axs[0, 0].plot(res.y[0].T, res.y[1].T, 'b')
+        axs[0, 1].plot(res.y[0].T, res.y[2].T, 'b')
+        axs[0, 2].plot(res.y[0].T, res.y[3].T, 'b')
+        axs[1, 0].plot(res.y[1].T, res.y[2].T, 'b')
+        axs[1, 1].plot(res.y[1].T, res.y[3].T, 'b')
+        axs[1, 2].plot(res.y[2].T, res.y[3].T, 'b')
+
+        axs[0, 0].set(xlabel='gamma1')
+        axs[0, 0].set(ylabel='gamma2')
+        axs[0, 0].grid()
+        axs[0, 1].set(xlabel='gamma1')
+        axs[0, 1].set(ylabel='gamma3')
+        axs[0, 1].grid()
+        axs[0, 2].set(xlabel='gamma1')
+        axs[0, 2].set(ylabel='gamma4')
+        axs[0, 2].grid()
+        axs[1, 0].set(xlabel='gamma2')
+        axs[1, 0].set(ylabel='gamma3')
+        axs[1, 0].grid()
+        axs[1, 1].set(xlabel='gamma2')
+        axs[1, 1].set(ylabel='gamma4')
+        axs[1, 1].grid()
+        axs[1, 2].set(xlabel='gamma3')
+        axs[1, 2].set(ylabel='gamma4')
+        axs[1, 2].grid()
+
+args = (0, 1000)
 
 fig, axs = plt.subplots(2, 3, figsize=(12, 7))
-drawPhasePortrait(args)
+sys = FiveOscillators(1, 1.6, -1.58, 0.2)
+sys.drawPhasePortrait(args)
 plt.show()
