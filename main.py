4f3822d9f3ed6b75747576c0f76a41ddf345593b
KasjanychevMikhail <43135863+KasjanychevMikhail@users.noreply.github.com>
2021-09-15 22:28:13 +0300

    ver5

--- main.py
+++ main.py
@@ -34,13 +34,14 @@ class FiveOscillators:
         return[self.paramW, self.paramA, self.paramB, self.paramR]
 
     def drawPhasePortrait(self, args):
-        res = solve_ivp(self.getReducedSystem, (args[0], args[1]), [3.6, 1.7, 1.2, -0.2])
-        axs[0, 0].plot(res.y[0].T, res.y[1].T, 'b')
-        axs[0, 1].plot(res.y[0].T, res.y[2].T, 'b')
-        axs[0, 2].plot(res.y[0].T, res.y[3].T, 'b')
-        axs[1, 0].plot(res.y[1].T, res.y[2].T, 'b')
-        axs[1, 1].plot(res.y[1].T, res.y[3].T, 'b')
-        axs[1, 2].plot(res.y[2].T, res.y[3].T, 'b')
+        res = solve_ivp(self.getReducedSystem, (args[0], args[1]), [3.6, 1.7, 1.2, -0.2],
+                        t_eval=np.linspace(0, 10000, 20000))
+        axs[0, 0].plot(res.y[0].T[8000:10000], res.y[1].T[8000:10000], 'b')
+        axs[0, 1].plot(res.y[0].T[8000:10000], res.y[2].T[8000:10000], 'b')
+        axs[0, 2].plot(res.y[0].T[8000:10000], res.y[3].T[8000:10000], 'b')
+        axs[1, 0].plot(res.y[1].T[8000:10000], res.y[2].T[8000:10000], 'b')
+        axs[1, 1].plot(res.y[1].T[8000:10000], res.y[3].T[8000:10000], 'b')
+        axs[1, 2].plot(res.y[2].T[8000:10000], res.y[3].T[8000:10000], 'b')
 
         axs[0, 0].set(xlabel='gamma1')
         axs[0, 0].set(ylabel='gamma2')
@@ -61,7 +62,7 @@ class FiveOscillators:
         axs[1, 2].set(ylabel='gamma4')
         axs[1, 2].grid()
 
-args = (0, 1000)
+args = (0, 10000)
 
 fig, axs = plt.subplots(2, 3, figsize=(12, 7))
 sys = FiveOscillators(1, 1.6, -1.58, 0.2)
